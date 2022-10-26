1ee135304ead3e2fcf09d37c230d496307a50978
avalanche284 <94930213+avalanche284@users.noreply.github.com>
2022-10-26 19:36:04 +0200

    next topic

--- IBM Analyzing Data with R/module 1/module1.R
+++ IBM Analyzing Data with R/module 1/module1.R
@@ -8,6 +8,36 @@ sub_airline <- read_csv("lax_to_jfk/lax_to_jfk.csv",
                           'DivDistance' = col_number(),
                           'DivArrDelay' = col_number()
                         ))
+head(sub_airline)
+tail(sub_airline)
+dim(sub_airline)
+colnames(sub_airline)
+# check data type
+sapply(sub_airline, typeof)
+x = 10.4
+class(x)
+k <- 1
+class(k)
+y <- as.integer(4)
+class(y)
+z <- 0i
+class(z)
+logical_values <- c(TRUE, T, FALSE, F)
+class(logical_values)
+class("this is a value")
+
+sub_airline %>%
+  group_by(Reporting_Airline) %>%
+  summarise(avg_carrier_delay = mean(CarrierDelay, na.rm = TRUE))
+
+sub_airline %>%
+  group_by(Reporting_Airline) %>%
+  summarize(sd_carrier_delay = sd(CarrierDelay, na.rm = TRUE))
 
 
+sub_airline %>%
+  group_by(Reporting_Airline) %>%
+  summarize(avg_arr_delay = mean(ArrDelay, na.rm = TRUE))
+
+glimpse(sub_airline)
 
